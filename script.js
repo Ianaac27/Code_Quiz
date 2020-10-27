0a147e971da6218d429545f20860dbea76daa5e4
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2020-10-27 14:46:16 -0700

    applying timer function

--- script.js
+++ script.js
@@ -10,3 +10,45 @@
 // WHEN the game is over
 // THEN I can save my initials and score
 //-----------------------------------------------------------
+
+var timer = document.getElementById("time");
+var mainHeader = document.querySelector(".main-header");
+var mainParagraph = document.querySelector(".main-paragraph");
+var startButton = document.getElementById("start");
+
+startButton.addEventListener("click", function(event) {
+    var secondsLeft = 75;
+    timer.textContent = "Timer= " + secondsLeft;
+    setInterval()
+
+    var timerInterval = setInterval(function() {
+    secondsLeft--;
+
+    if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+
+    }, 75000);  
+});
+
+// function setTime() {
+//     var timerInterval = setInterval(function() {
+//       secondsLeft--;
+  
+//       if(secondsLeft === 0) {
+//         clearInterval(timerInterval);
+//         sendMessage();
+//       }
+//     }, 75000);
+// }
+
+console.log(timer);
+
+function sendMessage() {
+    mainHeader.textContent = "Time Elapsed";
+    mainParagraph.textContent = "Please try again";
+  
+  }
+
+//   setTime();
