f9b6674ec48df7d49d8a75ca41c4d69e01e70aa4
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2020-10-31 03:07:15 -0700

    added final score display, opened a reset.css file, applied psuedocode

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
 // WHEN I answer a question
 // THEN I am presented with another question -------CHECK!!!
 // WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock --------- HAVE IDEA-
+// THEN time is subtracted from the clock --------- CHECK!!!
 // WHEN all questions are answered or the timer reaches 0
 // THEN the game is over ---------------------- CHECK!!!
 // WHEN the game is over
-// THEN I can save my initials and score ------------------NOT YET
+// THEN I can save my initials and score ------------------NOT YET -
 //-----------------------------------------------------------
 
 var timer = document.getElementById("time");
@@ -21,24 +21,28 @@ var answerButton3 = document.querySelector(".answer-button3")
 var answerButton4 = document.querySelector(".answer-button4")
 var previousAnswer = document.getElementById("previous");
 var tryAgain = document.getElementById("try-again");
+var highScores = document.getElementById("high-scores");
+var initials = document.getElementById("initials");
+var initialsButton = document.getElementById("initials-button");
 
 var correctAnswer= "Correct!";
 var wrongAnswer= "Wrong!";
-var score;
 
 var timerInterval;
 var secondsLeft = 61;
 
+// Starting the game
 startButton.addEventListener("click", function(event) {
     
     timerInterval = setInterval(function() {
     secondsLeft--;
     timer.textContent = "Time: " + secondsLeft;
 
+// Game Over when time runs out
      if(secondsLeft <= 0) {
        clearInterval(timerInterval);
        sendMessage();
-       score = secondsLeft - 50;
+       secondsLeft= - 50;
 
        console.log(score);
      }
@@ -48,15 +52,17 @@ startButton.addEventListener("click", function(event) {
     navigateFirstQuestion()
 });
 
+// Expired time display
 function sendMessage() {
-    mainHeader.textContent = "Time Elapsed";
+    mainHeader.textContent = "Time Expired";
     mainParagraph.textContent = "Yikes!";
     answerButton1.textContent = "Better";
     answerButton2.textContent = "Luck";
     answerButton3.textContent = "Next";
     answerButton4.textContent = "Time";
     tryAgain.textContent = "Click a button above if you want to try again!";
-    
+ 
+// Buttons can reload the page for a restart    
     answerButton1.addEventListener("click", function(event) {
       location.reload();
     }),
@@ -71,30 +77,32 @@ function sendMessage() {
     })
   }
 
+// Quiz questions  
 var questions = [
   { 
   'question': 'What color are apples?',
   'answers': ['Red','Blue','Purple','White'],
-  // 'correctAnswer': 0
+  // correct answer: 0
 }, {
   'question': 'How long is a mile?',
   'answers': ['450 ft','5280 ft','2580 ft','4500 ft'],
-  // 'correctAnswer': 1
+  // correct answer: 1
 }, {
   'question': 'What is the name of the football team based in Seattle?',
   'answers': ['Mariners','Cougars','Seahawks','Totems'],
-  // 'correctAnswer': 2
+  // correct answer: 2
 }, {
   'question': 'How many letters is the word Mississippi?',
   'answers': ['11','10','12','9'],
-  // 'correctAnswer': 2
+  // correct answer: 2
 }, {
   'question': 'Who was the 2nd President of the United States?',
   'answers': ['Thomas Jefferson','Abraham Lincoln','Andrew Jackson','John Adams'],
-  // 'correctAnswer': 3
+  // correct answer: 3
 }
 ];
 
+// First question display
 function navigateFirstQuestion() {
     mainParagraph.textContent = questions[0].question;
     answerButton1.textContent = questions[0].answers[0];
@@ -122,9 +130,9 @@ function navigateFirstQuestion() {
       previousAnswer.textContent = "Previous Answer: " + wrongAnswer;
       secondsLeft -= 5;
     })
-    // console.log(score);
   }
 
+  // Second question display
   function navigateSecondQuestion() {
       mainParagraph.textContent = questions[1].question;
       answerButton1.textContent = questions[1].answers[0];
@@ -154,6 +162,7 @@ function navigateFirstQuestion() {
       })
     }
 
+  // Third question display
     function navigateThirdQuestion() {
       mainParagraph.textContent = questions[2].question;
       answerButton1.textContent = questions[2].answers[0];
@@ -182,7 +191,8 @@ function navigateFirstQuestion() {
         secondsLeft - 1;
       })
     }
-
+  
+  // Fourth question display
     function navigateFourthQuestion() {
       mainParagraph.textContent = questions[3].question;
       answerButton1.textContent = questions[3].answers[0];
@@ -212,6 +222,7 @@ function navigateFirstQuestion() {
       })
     }
 
+  // Fifth question display
     function navigateFifthQuestion() {
       mainParagraph.textContent = questions[4].question;
       answerButton1.textContent = questions[4].answers[0];
@@ -221,28 +232,44 @@ function navigateFirstQuestion() {
   
       answerButton1.addEventListener("click", function(event) {
         clearInterval(timerInterval);
+        submitScore(); 
         previousAnswer.textContent = "Previous Answer: " + wrongAnswer;
-        score = secondsLeft - 1; 
-
+        secondsLeft -= 1;
       }),
 
       answerButton2.addEventListener("click", function(event) {
         clearInterval(timerInterval);
+        submitScore(); 
         previousAnswer.textContent = "Previous Answer: " + wrongAnswer;
-        score = secondsLeft - 1;  
+        secondsLeft -= 1; 
       }),
       
       answerButton3.addEventListener("click", function(event) { 
         clearInterval(timerInterval);
+        submitScore();
         previousAnswer.textContent = "Previous Answer: " + wrongAnswer;
-        score = secondsLeft - 1;  
+        secondsLeft -= 1;  
       }),
       
       answerButton4.addEventListener("click", function(event) {
         clearInterval(timerInterval);
+        submitScore();
         previousAnswer.textContent = "Previous Answer: " + correctAnswer;
-        score = secondsLeft + 1;
+        secondsLeft += 1;
       })
-
-      console.log(score);
     }
+
+  //Submit Score display
+    function submitScore() {
+      mainHeader.textContent = "Nice Work!";
+      mainParagraph.textContent = "Your Score: " + secondsLeft;
+      answerButton1.remove()
+      answerButton2.remove()
+      answerButton3.remove()
+      answerButton4.remove()
+
+      highScores.style.display = "block";
+    }
+      
+    
+  
